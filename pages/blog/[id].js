edbc9fef71cf94a0fd4a157ed3f2779d633e76f7
Deric Pang <pangd@google.com>
2022-12-28 13:49:48 -0800

    Remove images, add in latex support.

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,8 +1,12 @@
 import path from "path";
 import Image from "next/image";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
 import rehypeImgSize from "rehype-img-size";
+import rehypeKatex from "rehype-katex";
 import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote";
+import Head from "next/head";
 
 import Layout from "../../components/layout";
 import Date from "../../components/date";
@@ -20,7 +24,9 @@ export async function getStaticProps({ params }) {
   const { postContent, ...postData } = getPostData(params.id);
   const mdxSource = await serialize(postContent, {
     mdxOptions: {
-      rehypePlugins: [[rehypeImgSize, { dir: "public" }]],
+      remarkPlugins: [remarkGfm, remarkMath],
+      rehypePlugins: [[rehypeImgSize, { dir: "public" }], rehypeKatex],
+      development: false,
     },
   });
   return { props: { mdxSource, ...postData } };
@@ -86,27 +92,43 @@ const components = {
 
 export default function Post({ mdxSource, ...postData }) {
   return (
-    <Layout title={postData.title} description={postData.description}>
-      <h2 className="post-title">{postData.title}</h2>
-      <p className="post-date">
-        <Date dateString={postData.date} />
-      </p>
-      {postData.lastUpdated && (
-        <p className="last-updated-date">
-          Last updated on <Date dateString={postData.lastUpdated} />
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
+          integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0"
+          crossorigin="anonymous"
+        />
+        <script
+          defer
+          src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js"
+          integrity="sha384-PwRUT/YqbnEjkZO0zZxNqcxACrXe+j766U2amXcgMg5457rve2Y7I6ZJSm2A0mS4"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
+      <Layout title={postData.title} description={postData.description}>
+        <h2 className="post-title">{postData.title}</h2>
+        <p className="post-date">
+          <Date dateString={postData.date} />
         </p>
-      )}
-      {postData.featuredImage && (
-        <div className="post-featured-image">
-          <Image
-            src={featuredImage}
-            alt={postData.featuredImageAltText}
-            className="post-featured-image-img"
-            priority
-          />
-        </div>
-      )}
-      <MDXRemote {...mdxSource} components={components} />
-    </Layout>
+        {postData.lastUpdated && (
+          <p className="last-updated-date">
+            Last updated on <Date dateString={postData.lastUpdated} />
+          </p>
+        )}
+        {postData.featuredImage && (
+          <div className="post-featured-image">
+            <Image
+              src={featuredImage}
+              alt={postData.featuredImageAltText}
+              className="post-featured-image-img"
+              priority
+            />
+          </div>
+        )}
+        <MDXRemote {...mdxSource} components={components} />
+      </Layout>
+    </>
   );
 }
